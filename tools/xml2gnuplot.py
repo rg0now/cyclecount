2382c99585ec93a6e28f729f20490f243f0847a2
András Majdán <majdan.andras@gmail.com>
2013-12-12 01:12:29 +0100

    FIX: XML to gnuplot bugs

--- tools/xml2gnuplot.py
+++ tools/xml2gnuplot.py
@@ -28,76 +28,193 @@ if root.tag == 'group':
 	group = True
 else:
 	group = False
-	
-ywritten = False
-num=0
 
-with open(plotdata, 'w') as hdata, open(plotcmd, 'w') as hcmd:
-	hcmd.write("set boxwidth 0.5\n")
-	hcmd.write("set style fill solid\n")
-	hcmd.write('set yrange [0:*]\n');
-	hcmd.write('set title "' + root.attrib['name'] + '"\n')
+simple_one = False
+group_one = False
+
+datadict = {}
+
+with open(plotdata, 'w') as hdata:
+	title = root.attrib['name']
 	for child in root:
 		if (child.tag == 'single') and group:
 			for child2 in child:
 				if child2.tag == 'interval':
-					ivname = child2.attrib['name']
+					try:
+						xlabel
+					except NameError:
+						xlabel = child2.attrib['name']
+					else:
+						if xlabel != child2.attrib['name']:
+							print "ERROR: Comparing apples to oranges"
+							print xlabel + " instead of " + child2.attrib['name']
+							exit(1)
 					for child3 in child2:
 						if child3.tag == 'result':
 							if child3.attrib['type'] == 'median':
-								if not ywritten:
-									hcmd.write('set ylabel "' + child3.attrib['name'] + '"\n')
-									ywritten = True
-								towrite = str(num) + ' "' + child.attrib['name'] + ' - ' + child2.attrib['value'] + ' ' + ivname + '" ' + child3.text + "\n"
-								num += 1
-								hdata.write(towrite)
+								try:
+									ylabel
+								except NameError:
+									ylabel = child3.attrib['name']
+								else:
+									if ylabel != child3.attrib['name']:
+										print "ERROR: Comparing apples to oranges"
+										print ylabel + " instead of " + child3.attrib['name']
+										exit(1)
+								if child.attrib['name'] in datadict.keys():
+									datadict[child.attrib['name']].update( { child2.attrib['value'] : child3.text } )
+								else:
+									datadict[child.attrib['name']] = {}
+									datadict[child.attrib['name']].update( { child2.attrib['value'] : child3.text } )
 						else:
 							print "ERROR: Malformed XML\n"
 							exit(1)
 				else:
+					group_one = True
 					if child2.tag == 'result':
 						if child2.attrib['type'] == 'median':
-							if not ywritten:
-								hcmd.write('set ylabel "' + child2.attrib['name'] + '"\n')
-								ywritten = True
-							towrite = str(num) + ' "' + child.attrib['name'] + '" ' + child2.text + "\n"
-							num += 1
-							hdata.write(towrite)
+							try:
+								ylabel
+							except NameError:
+								ylabel = child2.attrib['name']
+							else:
+								if ylabel != child2.attrib['name']:
+									print "ERROR: Comparing apples to oranges"
+									print ylabel + " instead of " + child2.attrib['name']
+									exit(1)
+							datadict[child.attrib['name']] = child2.text
 					else:
 						print "ERROR: Malformed XML\n"
 						exit(1)
 		else:
 			if (child.tag == 'interval') and not group:
-				ivname = child.attrib['name']
+				try:
+					xlabel
+				except NameError:
+					xlabel = child.attrib['name']
+				else:
+					if xlabel != child.attrib['name']:
+						print "ERROR: Comparing apples to oranges"
+						print xlabel + " instead of " + child.attrib['name']
+						exit(1)
 				for child2 in child:
 					if child2.tag == 'result':
 						if child2.attrib['type'] == 'median':
-							if not ywritten:
-								hcmd.write('set ylabel "' + child2.attrib['name'] + '"\n')
-								hcmd.write('set xlabel "' + ivname + '"\n')
-								ywritten = True
-							towrite = str(num) + ' "' + child.attrib['value'] + '" ' + child2.text + "\n"
-							num += 1
-							hdata.write(towrite)
+							try:
+								ylabel
+							except NameError:
+								ylabel = child2.attrib['name']
+							else:
+								if ylabel != child2.attrib['name']:
+									print "ERROR: Comparing apples to oranges"
+									print ylabel + " instead of " + child2.attrib['name']
+									exit(1)
+							datadict[child.attrib['value']] = child2.text
 					else:
 						print "ERROR: Malformed XML\n"
 						exit(1)					
 			else:
 				if (child.tag == 'result') and not group:
 					if child.attrib['type'] == 'median':
-						if not ywritten:
-							hcmd.write('set ylabel "' + child.attrib['name'] + '"\n')
-							ywritten = True
-						towrite = '0 0 ' + child.text + "\n"
-						hdata.write(towrite)
+						simple_one = True
+						try:
+							ylabel
+						except NameError:
+							ylabel = child.attrib['name']
+						else:
+							if ylabel != child.attrib['name']:
+								print "ERROR: Comparing apples to oranges"
+								print ylabel + " instead of " + child.attrib['name']
+								exit(1)
+						simple_one_text = child.text
 				else:
 					print "ERROR: Malformed XML\n"
 					exit(1)
-	hcmd.write("set terminal pngcairo size 800,600 enhanced font 'Verdana,10'\n")
-	hcmd.write('set output "plot.png"\n');
-	hcmd.write('plot "plot.dat" using 1:3:xtic(2) with boxes\n')
-	hcmd.write("set terminal postscript eps enhanced color font 'Helvetica,10'\n")
-	hcmd.write("set output 'plot.eps'\n")
-	hcmd.write('plot "plot.dat" using 1:3:xtic(2) with boxes\n')
+
+	newdict = {}
+	key_num = 0
+	val_curnum = 0
+	val_maxnum = 0
+	
+	hdata.write('no')
+	if not group:
+		hdata.write("\t" + title)
+	
+	if simple_one:
+		val_maxnum = 2
+		key_num = 1
+		hdata.write("\n")
+		hdata.write("x " + simple_one_text + "\n")
+		xlabel = ""
+	elif group_one:
+		val_maxnum = 0
+		key_num = 1
+		xlabel = ""
+		hdata.write("\t" + title + "\n")
+		for key1 in sorted(datadict.iterkeys()):
+			hdata.write(str(key1) + "\t" + str(datadict[key1] + "\n"))
+			key_num += 1
+	else:
+		for key1 in sorted(datadict.iterkeys()):
+			if group:
+				val_curnum = 0
+				key_num += 1
+				hdata.write("\t" + key1)
+				for key2 in sorted(datadict[key1].iterkeys(), key=int):
+					if key2 not in newdict.keys():
+						newdict[key2] = {}
+					newdict[key2].update( { key1 : datadict[key1][key2] } )
+					val_curnum += 1
+					if val_curnum > val_maxnum:
+						val_maxnum = val_curnum
+			else:
+				key_num = 1
+				val_maxnum += 1
+				newdict[key1] = datadict[key1]
+
+		hdata.write("\n")
+
+		for key1 in sorted(newdict.iterkeys(), key=int):
+			hdata.write(str(key1))
+			if group:
+				for key2 in sorted(datadict.iterkeys()):
+					hdata.write("\t")
+					if key2 not in newdict[key1].keys():
+						hdata.write("-")
+					else:
+						hdata.write(str(newdict[key1][key2]))
+			else:
+				hdata.write("\t" + str(newdict[key1]))
+			hdata.write("\n")
 				
+graph_width = 20 * val_maxnum * (key_num/2+1) + 200
+graph_height = 500
+
+with open(plotcmd, 'w') as hcmd:
+	hcmd.write('set terminal pngcairo enhanced font "arial,10" fontscale 1.0 size ')
+	hcmd.write(str(graph_width) + ', ' + str(graph_height) + "\n")
+	hcmd.write("""set output 'plot.png'
+set boxwidth 0.9 absolute
+set style fill   solid 1.00 border lt -1
+set key autotitle columnheader
+set key invert reverse Left outside
+set style histogram clustered gap 1 title  offset character 0, 0, 0
+set datafile missing '-'
+set yrange [0:*]
+set style data histograms""" + "\n") 
+	hcmd.write('set ylabel' + ' "' + ylabel + '"\n')
+	if xlabel != "":
+		hcmd.write('set xlabel' + ' "' + xlabel + '"\n')
+	hcmd.write('set title' + ' "' + title + '"\n')
+	if key_num < 2 or group_one:
+		hcmd.write("set nokey\n")
+		if group_one:
+			hcmd.write("set xtics nomirror rotate by -90\n")
+		hcmd.write("plot 'plot.dat' using 2:xtic(1)\n")
+	else:
+		hcmd.write("plot 'plot.dat' using 2:xtic(1), for [i=3:" + str(key_num+1) + "] '' using i\n")
+		
+	hcmd.write('set terminal postscript eps enhanced color font "arial,8"\n')
+	hcmd.write("set output 'plot.eps'\n")
+	hcmd.write("replot\n")
 			
